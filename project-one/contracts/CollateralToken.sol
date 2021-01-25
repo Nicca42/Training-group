7f76dc40ed6122a9fbf71bebcd9ca7e2bfaa57fe
William <william@linumlabs.com>
2021-01-25 16:39:47 +0200

    initial commit

--- project-one/contracts/CollateralToken.sol
+++ project-one/contracts/CollateralToken.sol
@@ -4,18 +4,28 @@ pragma solidity ^0.7.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CollateralToken is ERC20 {
-    constructor() 
+    address public deployer;
+    address public curve;
+
+    modifier onlyDeployerOrCurve() {
+        require(msg.sender == deployer || msg.sender == curve, "unauthorized address");
+        _;
+    }
+
+    constructor(address _curve) 
     ERC20(
         "Collateral Token",
         "CLT"
     ) {
-
+        deployer = msg.sender;
+        curve = _curve;
     }
-    function mint(address account, uint256 amount) public {
+
+    function mint(address account, uint256 amount) public onlyDeployerOrCurve {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) public {
+    function burn(address account, uint256 amount) public onlyDeployerOrCurve {
         _burn(account, amount);
     }
 }
